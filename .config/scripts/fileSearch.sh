354252b915e4102becf9dbb769078a52f721252b
Almalexia <humanity.lost97@gmail.com>
2024-02-23 14:06:58 -0500

    increased file search performance

--- .config/scripts/fileSearch.sh
+++ .config/scripts/fileSearch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #there are two options for this script, one will omit hidden files
-[ "$1" = "hidden" ] && FILE=$(locate ~ | rofi -threads 0  --keep-right -dmenu -i)
-[ "$1" != "hidden" ] && FILE=$(locate -0 ~ | awk -v RS='\0' '!/\/\./' | rofi -threads 0  --keep-right -dmenu -i)
+[ "$1" = "hidden" ] && FILE=$(find ~ | rofi -threads 0  --keep-right -dmenu -i)
+[ "$1" != "hidden" ] && FILE=$(find ~ -not -path '*/\.*' | rofi -threads 0  --keep-right -dmenu -i)
 
 #save the above output as a variable first, then query what .desktop opens it
 #if that desktop file has terminal=true, hit that shit with alacritty -e
